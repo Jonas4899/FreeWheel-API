58dbb18ecd966b8aba0af14c4fc4b67858805fc6
Jonathan David Salcedo Diaz <jonatandavidsal@gmail.com>
2025-03-22 10:27:39 -0500

    Add initial commit

---
+++ src/main/java/com/freewheel/FreeWheelBackend/FreeWheelBackendApplication.java
@@ -0,0 +1,13 @@
+package com.freewheel.FreeWheelBackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FreeWheelBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FreeWheelBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/freewheel/FreeWheelBackend/FreeWheelBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.freewheel.FreeWheelBackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FreeWheelBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
