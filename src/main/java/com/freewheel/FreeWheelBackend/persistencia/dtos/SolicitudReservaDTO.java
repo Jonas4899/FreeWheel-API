364ce635e565d26818d302388210c84a3d95273e
Juan Felipe <juanfeliperomerocastro09@gmail.com>
2025-05-22 13:32:18 -0500

    refactor: add asientos disponibles info

--- src/main/java/com/freewheel/FreeWheelBackend/persistencia/dtos/SolicitudReservaDTO.java
+++ src/main/java/com/freewheel/FreeWheelBackend/persistencia/dtos/SolicitudReservaDTO.java
@@ -22,4 +22,6 @@ public class SolicitudReservaDTO {
     private ZonedDateTime fechaRespuesta;
     private String mensajePasajero;
     private String mensajeConductor;
+
+    private int asientosSolicitados;
 }
